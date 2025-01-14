11eff3f67bb4bc4b3ca3dce57d7bad4c2e60ede2
Mathis LEBEL <145991354+mathislebel@users.noreply.github.com>
2025-01-14 15:33:47 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+/*
+today I build a data managment solution, similar to a my previous project
+exercise, based on binary search tree.
+      you can use an auxiliary data structure too...
+
+    supported action: Search/retrieval, Insert, Delete
+
+    the tree should be self-balancing
+        what data should you store inside it ?
+
+    for that I code on c++ language and use the following libraries:
+    - iostream
+    - string
+    - vector
+    - algorithm
+
+
+    I will use the following data structure:
+    - struct Node
+    - class BST
+    
+
+    FOR THIS PROJECT I CREATE a file name :
+    - main.cpp
+    - bst.h
+    - bst.cpp
+    - node.h
+    - node.cpp
+    - Makefile
+
+    node for the node structure and bst for the binary search tree structure
+    
+    node dois etre un code pour la structure de noeud
+
+    bst dois etre un code pour la structure de l'arbre binaire de recherche
+    
+    on firts step I create a node.h and node.cpp file
+
+
+
+*/
